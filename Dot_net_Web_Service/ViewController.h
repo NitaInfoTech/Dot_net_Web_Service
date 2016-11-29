8f8ffb40e0d9ed90a57058cc83f5e1841afc0311
MACOS <macos@macoss-imac-26.local>
2016-11-29 08:32:03 +0530

    Initial Commit

---
+++ Dot_net_Web_Service/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dot_net_Web_Service
+//
+//  Created by MACOS on 11/29/16.
+//  Copyright © 2016 MACOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Dot_net_Web_Service/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Dot_net_Web_Service
+//
+//  Created by MACOS on 11/29/16.
+//  Copyright © 2016 MACOS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
